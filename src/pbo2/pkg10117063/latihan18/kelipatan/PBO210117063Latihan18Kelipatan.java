abe2366b47993b6af9f3fdeffc16127ca4f5b8b1
cessario <caesarsheva23@gmail.com>
2018-10-01 15:04:40 +0700

    Initial commit

---
+++ src/pbo2/pkg10117063/latihan18/kelipatan/PBO210117063Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan18.kelipatan;
+
+/**
+ *
+ * @author DEADPOOL
+ */
+public class PBO210117063Latihan18Kelipatan {
+
+    /*  Nama : Cessario Sheva Lakita Purwa Adidjaya
+        NIM  : 10117063
+        Kelas : IF-2
+        Deskripsi Program : Menampilkan kelipatan 3.5
+     */
+   
+    public static void main(String[] args) {
+       double i = 3.5;
+       do{
+           if(i>=3.5) {
+               System.out.println(i+"");
+           } 
+            i+=3.5;
+            
+       }while(i<=35);
+          
+
+
+        }
+
+       }
